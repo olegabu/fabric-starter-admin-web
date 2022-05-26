99bb353579dd5b5144ef9e36a5d1098ffbd474af
leonidle <>
2022-05-26 20:30:55 +0300

    script for packing app into tgz

---
+++ pack-admin-webapp.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+packageName=admin-webapp.tgz
+rm -f ${packageName}
+
+npm install
+au build --env prod
+echo "Built"
+
+tar -zcvf ${packageName} index.html scripts
+echo "Package prepared"
+
+cp -v ${packageName} ../fabric-starter-rest
